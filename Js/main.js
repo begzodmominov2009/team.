ed04e67f8359b24d8f72a7243a37ecc1a601ec1a
begzodmominov2009 <begzodmominov32@gmail.com>
2025-07-28 01:08:50 +0300

    adding register

--- Js/main.js
+++ Js/main.js
@@ -2,6 +2,13 @@ const MenuToggle = document.getElementById("toggle");
 const NavRes = document.getElementById("nav_res");
 const close = document.getElementById("close");
 const header = document.getElementById("header");
+const loading = document.getElementById("loading");
+
+
+window.addEventListener("load" , () => {
+    loading.classList.add("stop-load")
+} , 3000)
+
 MenuToggle.addEventListener("click", () => {
     NavRes.classList.add("show");
 })
@@ -10,10 +17,33 @@ close.addEventListener("click", () => {
     NavRes.classList.remove("show");
 })
 
-window.addEventListener("scroll" , () => {
-    if(scrollY > 110){
+window.addEventListener("scroll", () => {
+    if (scrollY > 50) {
         header.classList.add("shrink");
-    }else{
+    } else {
         header.classList.remove("shrink");
     }
 })
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay:true,
+    autoplayTimeour: 50,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+
