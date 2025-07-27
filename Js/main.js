032c4ae1923ebb1e0ca8cbc854af21edfa24f2a3
begzodmominov2009 <begzodmominov32@gmail.com>
2025-07-27 11:51:27 +0300

    adding project

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,19 @@
 const MenuToggle = document.getElementById("toggle");
 const NavRes = document.getElementById("nav_res");
 const close = document.getElementById("close");
-
+const header = document.getElementById("header");
 MenuToggle.addEventListener("click", () => {
     NavRes.classList.add("show");
 })
 
 close.addEventListener("click", () => {
     NavRes.classList.remove("show");
+})
+
+window.addEventListener("scroll" , () => {
+    if(scrollY > 110){
+        header.classList.add("shrink");
+    }else{
+        header.classList.remove("shrink");
+    }
 })
